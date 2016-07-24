586aaac16a31f122578d21245447bf8d482fbd88
Xiaolong Wang <xiaolonw@cs.cmu.edu>
2016-07-23 23:32:58 -0400

    demo

--- tools/demo_cars_cocomodel.py
+++ tools/demo_cars_cocomodel.py
@@ -159,10 +159,8 @@ if __name__ == '__main__':
 
     args = parse_args()
 
-    prototxt = os.path.join(cfg.MODELS_DIR, NETS[args.demo_net][0],
-                            'faster_rcnn_alt_opt', 'faster_rcnn_test.pt')
-    caffemodel = os.path.join(cfg.DATA_DIR, 'faster_rcnn_models',
-                              NETS[args.demo_net][1])
+    prototxt = '/nfs.yoda/xiaolonw/faster_rcnn/xiaolonw/py-faster-rcnn2/models/coco/VGG16/faster_rcnn_end2end/test.prototxt'
+    caffemodel = '/nfs.yoda/xiaolonw/faster_rcnn/xiaolonw/py-faster-rcnn2/data/faster_rcnn_models/vgg16_faster_rcnn_iter_490000_coco.caffemodel'
 
     if not os.path.isfile(caffemodel):
         raise IOError(('{:s} not found.\nDid you run ./data/script/'
