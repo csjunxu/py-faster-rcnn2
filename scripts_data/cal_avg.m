9a1be343a1ebc7584f1f91f6e246e608c162f2ad
Xiaolong Wang <xiaolonw@cs.cmu.edu>
2016-07-31 13:29:17 -0400

    test

--- scripts_data/cal_avg.m
+++ scripts_data/cal_avg.m
@@ -5,7 +5,7 @@ txffile = 'scores.txt';
 
 fid = fopen(txffile, 'r');
 sumscore =  0
-for i = 1 : 19
+for i = 1 : 20
 	s = fscanf(fid, '%s', 1);
 	s = fscanf(fid, '%s', 1);
 	score = fscanf(fid, '%f', 1);
@@ -15,5 +15,5 @@ end
 
 fclose(fid);
 
-sumscore = sumscore / 19
+sumscore = sumscore / 20
 
