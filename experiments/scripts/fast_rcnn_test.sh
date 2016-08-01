2a9a4dd4c8956c6b490fedc7f1315166121373ce
Xiaolong Wang <xiaolonw@cs.cmu.edu>
2016-08-01 14:23:24 -0400

    neg

--- experiments/scripts/fast_rcnn_test.sh
+++ experiments/scripts/fast_rcnn_test.sh
@@ -49,7 +49,7 @@ esac
 
 
 set +x
-NET_FINAL='/nfs.yoda/xiaolonw/faster_rcnn/xiaolonw/py-faster-rcnn2/output/fast_rcnn_pascal/voc3_2007_trainval/save/vgg_cnn_m_1024_fast_rcnn2_2_iter_40000.caffemodel'
+NET_FINAL='/nfs.yoda/xiaolonw/faster_rcnn/xiaolonw/py-faster-rcnn2/output/fast_rcnn_pascal/voc3_2007_trainval/vgg_cnn_m_1024_fast_rcnn2_2double_iter_80000.caffemodel'
 set -x
 
 time ./tools/test_net.py --gpu ${GPU_ID} \
@@ -57,7 +57,5 @@ time ./tools/test_net.py --gpu ${GPU_ID} \
   --net ${NET_FINAL} \
   --imdb ${TEST_IMDB} \
   ${EXTRA_ARGS}
-
-#   --def models/${PT_DIR}/${NET}/fast_rcnn/test.prototxt \
 
 
